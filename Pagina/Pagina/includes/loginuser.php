074b17d6c201ac8c0def70e4530305a2b12cb9d6
Moha2001B <molamedb@gmail.com>
2021-10-19 18:37:40 +0200

    Implementat el login i la conexio amb php

--- Pagina/Pagina/includes/loginuser.php
+++ Pagina/Pagina/includes/loginuser.php
@@ -3,7 +3,7 @@
     $username='kirll';
     $password='Nemes1sx';
     $dbname = "hotel";
-    $conn=mariadb_connect($servername,$username,$password,"$dbname");
+    $conn=mysqli_connect($servername,$username,$password,"$dbname");
       if(!$conn){
           die('Could not Connect MySql Server:' .mysql_error());
         }
