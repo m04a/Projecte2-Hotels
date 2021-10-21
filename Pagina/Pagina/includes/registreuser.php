55810f2920b6a204b850b88a25f89963c3dfd458
Moha2001B <molamedb@gmail.com>
2021-10-21 16:35:25 +0200

    Implementat el login i la conexio amb php

--- Pagina/Pagina/includes/registreuser.php
+++ Pagina/Pagina/includes/registreuser.php
@@ -9,10 +9,7 @@ require "conectar_DB.php";
 
   $password = validate($_POST['password']);
 
- $query = "INSERT INTO usuario (IDusuario, password) VALUES('$usuari','$password')";
-  	
- mysqli_query($db, $query);
- $sql = "INSERT INTO usuario (usuari,password) VALUES (:usuari,:password)";
+ $sql = "INSERT INTO usuario (IDusuario,password) VALUES (:usuari,:password)";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':usuari', $_POST['IDusuario']);
 	$stmt->bindParam(':password', $_POST['password']);
