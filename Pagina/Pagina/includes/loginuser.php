e3da33a2a052864ae9c3ed1147fa4c038996f9c4
Moha2001B <molamedb@gmail.com>
2021-10-22 16:21:05 +0200

    Login fet, adminstrador habitacions beta i recerca de habitacions

--- Pagina/Pagina/includes/loginuser.php
+++ Pagina/Pagina/includes/loginuser.php
@@ -1,21 +1,23 @@
-<!DOCTYPE html>
-  <?php
+<?php
 
-include 'conectar_DB.php';
+require 'conectar_DB.php';
 
 if(!isset($_POST['usuari'],$_POST['password'])){
-    echo('Location:login.php');
-    header('Location:login.php');
+    echo('Location:../login.php');
+    header('Location:../login.php');
 }
-$sql= "SELECT tipo FROM usuario WHERE usuari='".$_POST['usuari']."' AND password='".$_POST['password']."' LIMIT 1;";
+$usuari = $_POST['usuari']; 
+$password = $_POST['password']; 
+
+$sql= "SELECT tipo FROM usuario WHERE usuari='$usuari' AND password='$password' LIMIT 1;";
 
 $rol = $conn->query($sql);
 
     while($row = $rol->fetch_assoc()) {
         if($row['rol']=='cliente'){
-            header('Location:/index.php');
+            header('Location:../index.php');
         } else{
-            header('Location:admin/admin.php');
+            header('Location:../admin/admin.php');
         }
         
     }
--- Pagina/Pagina/login.php
+++ Pagina/Pagina/login.php
@@ -1,28 +1,4 @@
-  <?php
-
-require 'includes/conectar_DB.php';
-
-if(!isset($_POST['usuari'],$_POST['password'])){
-    echo('Location:login.php');
-    header('Location:login.php');
-}
-$usuari = $_POST['usuari']; 
-$password = $_POST['password']; 
-
-$sql= "SELECT tipo FROM usuario WHERE usuari='$usuari' AND password='$password' LIMIT 1;";
-
-$rol = $conn->query($sql);
-
-    while($row = $rol->fetch_assoc()) {
-        if($row['rol']=='cliente'){
-            header('Location:index.php');
-        } else{
-            header('Location:admin/admin.php');
-        }
-        
-    }
- ?>
-<!DOCTYPE html>
+  <!DOCTYPE html>
 <html lang="en">
   <head>
 
