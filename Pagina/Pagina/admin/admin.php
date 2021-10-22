9f0274bb630c513702d2eb3d855ff5c3f3880436
Moha2001B <molamedb@gmail.com>
2021-10-22 11:44:40 +0200

    Afegeit generador de habitacions

---
+++ Pagina/Pagina/admin/admin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+    <title>Pagina Hotel| Admin Panel </title>
+
+    <link rel="stylesheet" type="text/css" href="utilitats/css/font-awesome.css">
+
+    <link rel="stylesheet" href="utilitats/css/adminstyle.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    </head>
+    
+    <body> 
+    
+
+
+  </body>
+</html>
