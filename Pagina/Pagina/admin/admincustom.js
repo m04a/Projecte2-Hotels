8b670b88036940f9a4ea42c00e3828dbb2a73c01
Moha2001B <molamedb@gmail.com>
2021-10-27 18:38:07 +0200

    canvis

--- Pagina/Pagina/admin/admincustom.js
+++ Pagina/Pagina/admin/admincustom.js
@@ -2,8 +2,8 @@ document.getElementById("crearhabitacio").onclick = function() {crearhabitacio()
 document.getElementById("crearusuari").onclick = function() {crearusuari()};
 
 function crearhabitacio() {
-  document.getElementById("div-crearhabitacio").style.display = 'none';
-  document.getElementById("div-crearusuari").style.display = 'block';
+  document.getElementById("div-crearhabitacio").style.display = 'block';
+  document.getElementById("div-crearusuari").style.display = 'none';
 
 }
 function crearusuari() {
