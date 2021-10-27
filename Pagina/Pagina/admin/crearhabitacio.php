101a8794dc6a5e962888cfe76beef339a089befc
Moha2001B <molamedb@gmail.com>
2021-10-27 18:30:23 +0200

    canvis

--- Pagina/Pagina/admin/crearhabitacio.php
+++ Pagina/Pagina/admin/crearhabitacio.php
@@ -127,8 +127,8 @@
 </form>
 </div> 
 		
-    <script src="admincustom.js"></script>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+   <!-- <script src="admincustom.js"></script>
+     jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
  
 <!-- Latest compiled and minified Bootstrap JavaScript -->
