371af7a35c2353553110536cf5d017b99a320ed3
nemesxv <lupenkov14@gmail.com>
2021-10-27 15:52:27 +0200

    editado html

--- Pagina/Pagina/admin/crearhabitacio.php
+++ Pagina/Pagina/admin/crearhabitacio.php
@@ -69,7 +69,7 @@ if($_POST){
 
     <link rel="stylesheet" type="text/css" href="utilitats/css/font-awesome.css">
 
-    <link rel="stylesheet" href="adminstyle.css">
+   <!-- <link rel="stylesheet" href="adminstyle.css"> -->
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
