f66e1849f7de362e6849c25ede598b9451fcbd2f
Moha2001B <molamedb@gmail.com>
2021-11-02 18:38:56 +0100

    Millores PHP

---
+++ Pagina/Pagina/includes/recercaReserva.php
@@ -0,0 +1,28 @@
+<div class="buscador">
+  <form class="buscadorForm" submit="reservabuscador.php">
+<label for="from">Des de</label>
+<input type="text" id="from" name="from">
+<label for="to">Fins a </label>
+<input type="text" id="to" name="to">
+<!-- *** Calendari Final *** -->
+
+<!-- *** Seleccionar tipus habitació i numero persones Final *** -->
+<label for="tipus">Numero de persones:</label>
+  <select name="npersones" id="npersones">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+  <option value="4">4</option>
+  </select>
+
+<!-- *** Seleccionar tipus habitació i numero persones Inici *** -->
+<label for="tipus">Nombre de habitacions</label>
+  <select name="nhabitacio" id="nhabitacio">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+  </select>
+  <input type="submit" value="submit">
+  </form>
+  </div>
--- Pagina/Pagina/reservabuscador.php
+++ Pagina/Pagina/reservabuscador.php
@@ -44,31 +44,9 @@
 <!-- *** Reserva Final *** -->
 <!-- *** Calendari Inici *** -->
 
-<div class="buscador">
-  <form submit="reservabuscador.php">
-<label for="from">Des de</label>
-<input type="text" id="from" name="from">
-<label for="to">Fins a </label>
-<input type="text" id="to" name="to">
-<!-- *** Calendari Final *** -->
-
-<!-- *** Seleccionar tipus habitació i numero persones Inici *** -->
-<label for="tipus">Nombre de habitacions</label>
-  <select name="nhabitacio" id="nhabitacio">
-    <option value="1">1</option>
-    <option value="2">2</option>
-  </select>
-<!-- *** Seleccionar tipus habitació i numero persones Final *** -->
-<label for="tipus">Numero de persones:</label>
-  <select name="npersones" id="npersones">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-	<option value="4">4</option>
-  </select>
-  <input type="submit" value="submit">
-  </form>
-  </div>
+<?php
+   include 'includes/recercaReserva.php';
+  ?>
 <!-- *** Reserva final *** -->
 
 
