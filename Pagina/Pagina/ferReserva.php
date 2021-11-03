3d7e2c83a3b2f9b15b3db9aebcde58bdd93d670e
Moha2001B <molamedb@gmail.com>
2021-11-03 16:47:34 +0100

    Afegits containers habitacions

--- Pagina/Pagina/ferReserva.php
+++ Pagina/Pagina/ferReserva.php
@@ -4,7 +4,7 @@
     <title>Veure habitació - Admin</title>
  
     <!-- Latest compiled and minified Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <link rel="stylesheet" href="utilitats/css/style.css">
     <link rel="stylesheet" href="utilitats/css/font-awesome.css">
