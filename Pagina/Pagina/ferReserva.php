0694b9acd5752b4325b776d398f58cb69992e5f9
Moha2001B <molamedb@gmail.com>
2021-11-03 16:25:27 +0100

    Afegits containers habitacions

--- Pagina/Pagina/ferReserva.php
+++ Pagina/Pagina/ferReserva.php
@@ -6,7 +6,7 @@
     <!-- Latest compiled and minified Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 
-    <link rel="stylesheet" href="adminstyle.css">
+    <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
