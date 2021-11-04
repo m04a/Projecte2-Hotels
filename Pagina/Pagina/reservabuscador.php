b6cb6ef845ea342095da39ad6cea2c6a3af501da
Moha2001B <molamedb@gmail.com>
2021-11-04 17:02:53 +0100

    Fixed footer afegit logo

--- Pagina/Pagina/reservabuscador.php
+++ Pagina/Pagina/reservabuscador.php
@@ -87,7 +87,7 @@ LA HABITACIÓ NO ESTÁ RESERVADA EN ELS PERIODES DEMANATS **/
                             </p>
 
                             <ul class="social-icons">
-                                <li><?php echo "<a href='ferReserva.php?numhab={$numhab}' class='btn btn-info m-r-1em'>Llegir</a>";?> </li>
+                                <li><?php echo "<a href='ferReserva.php?numhab={$numhab}' class='btn btn-info m-r-6em'>Reservar</a>";?> </li>
                             </ul>
                         </div>
                     </div>
