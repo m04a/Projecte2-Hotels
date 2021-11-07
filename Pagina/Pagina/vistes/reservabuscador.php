00b5375ea1a7c1daebf0ab2ec2dcb16dc9324b66
Moha2001B <molamedb@gmail.com>
2021-11-07 13:20:01 +0100

    Canvi get per post

--- Pagina/Pagina/vistes/reservabuscador.php
+++ Pagina/Pagina/vistes/reservabuscador.php
@@ -59,11 +59,7 @@ $to = $_POST["to"];
 $from = $_POST["from"];
 $nhabitacions = $_POST["nhabitacions"];
 $npersones = $_POST["npersones"];
-echo $to;
-echo $from;
-echo $nhabitacions;
-echo $npersones;
-    if(!empty($_POST["to"])){
+    if(!empty($to) && !empty($from) && !empty($nhabitacions) !empty($npersones)){
    //Hem de posar les condicions corresponentes
     /*HABITACIÓ NO TOPA AMB EL PERIODE DE VACANCES DEL HOTEL
 HI HAN SUFICIENTS HABITACIONS DE CADA TIPUS
