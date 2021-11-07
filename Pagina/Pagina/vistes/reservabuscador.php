f2d9518393d69bc508d6eb8e68e1de54bdaeb239
Moha2001B <molamedb@gmail.com>
2021-11-07 17:01:58 +0100

    Motor de reserva operatiu

--- Pagina/Pagina/vistes/reservabuscador.php
+++ Pagina/Pagina/vistes/reservabuscador.php
@@ -98,7 +98,7 @@ LA HABITACIÓ NO ESTÁ RESERVADA EN ELS PERIODES DEMANATS **/
 
                             <ul class="social-icons">
                                 <li>
-                                    <?php echo '<form action="ferReserva.php?idtipo={.'$idtipo'.}" method="post">'; ?>
+                                    <form action=<?php echo '"ferReserva.php?idtipo={$idtipo}"'; ?> method="post">
                                     <input type="hidden" name="desde" value="<?php echo $from;?>" />
                                     <input type="hidden" name="fins" value="<?php echo $to;?>" />
                                     <button type="submit" class='btn btn-info m-r-6em'>Reservar</button> </li>
