cee7fbb7b6db8b2295372f2d7a8fbb6fca7815ff
Moha2001B <molamedb@gmail.com>
2021-11-07 16:13:28 +0100

    Motor de reserva operatiu

--- Pagina/Pagina/vistes/reservabuscador.php
+++ Pagina/Pagina/vistes/reservabuscador.php
@@ -72,11 +72,10 @@ LA HABITACIÓ NO ESTÁ RESERVADA EN ELS PERIODES DEMANATS **/
     echo '<div class="row">';
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
         extract($row); 
-        $query2 = "SELECT COUNT('{$idtipo}') FROM reserva WHERE finicio <= '{$from}' AND ffin => '{$to}'";
-        $stmt1 = $conn->prepare($query2); 
-        $resultat = mysql_fetch_row($stmt1);
-
-        $counthab = $resultat[0];
+        //$query2 = "SELECT COUNT('{$idtipo}') FROM reserva WHERE finicio <= '{$from}' AND ffin => '{$to}'";
+        //$stmt1 = $conn->prepare($query2); 
+        //$resultat = mysql_fetch_row($stmt1);
+        //$counthab = $resultat[0];
 
         echo $counthab;
         ?>
