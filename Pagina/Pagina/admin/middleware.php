6735ffdc5f2a974b62a9737bc740655118d60681
Moha2001B <molamedb@gmail.com>
2021-11-08 19:33:18 +0100

    afegit adminpanel en nav

--- Pagina/Pagina/admin/middleware.php
+++ Pagina/Pagina/admin/middleware.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-      if($_SESSION["tipo"]!="cliente"){
+      if($_SESSION["tipo"]="cliente"){
       			echo 'Estas accedint en una pagina prohbida';
                 header('Refresh: 10; URL=../includes/index.php');
                 }
