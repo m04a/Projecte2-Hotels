28c518b79c5609bf7de3129b9aae6f0fc0fdaf9f
Moha2001B <molamedb@gmail.com>
2021-11-08 23:22:09 +0100

    Encriptar passwords

---
+++ Pagina/Pagina/includes/dologin.php
@@ -0,0 +1,35 @@
+  <?php
+  session_start();
+
+  $message = '';
+  function validate($data){
+       $data = trim($data);
+       $data = stripslashes($data);
+       $data = htmlspecialchars($data);
+       return $data;
+}
+  if (isset($_POST["login"])) {
+      $usuari = $_POST['usuari']; 
+      validate($usuari);
+      $password = $_POST['password'];
+      validate($password);
+      
+
+    $hash =$conn->query("SELECT password FROM usuario WHERE usuari ='$usuari'")->fetchColumn();
+    validate($hash);
+    $tipo =$conn->query("SELECT tipo FROM usuario WHERE usuari ='$usuari'")->fetchColumn();
+
+if (password_verify($password, $hash)) {
+        $_SESSION["usuari"] = $usuari;
+        $_SESSION["tipo"] =$tipo;
+         header("location: index.php");
+      }
+      else{
+     $message = 'El usuari es incorrecte';
+    header("Location:login.php");
+}
+  if(isset($_SESSION["usuari"])) {
+    header("Location:index.php");
+    }
+}
+?>
--- Pagina/Pagina/vistes/login.php
+++ Pagina/Pagina/vistes/login.php
@@ -1,38 +1,7 @@
 <?php
 
 require '../includes/conectar_DB.php';
-
-   session_start();
-
-  $message = '';
-  function validate($data){
-       $data = trim($data);
-       $data = stripslashes($data);
-       $data = htmlspecialchars($data);
-       return $data;
-}
-  if (isset($_POST["login"])) {
-      $usuari = $_POST['usuari']; 
-      validate($usuari);
-      $password = $_POST['password'];
-      validate($password);
-      
-
-    $hash =$conn->query("SELECT password FROM usuario WHERE usuari ='$usuari'")->fetchColumn();
-    validate($hash);
-    $tipo =$conn->query("SELECT tipo FROM usuario WHERE usuari ='$usuari'")->fetchColumn();
-
-if (password_verify($password, $hash)) {
-        $_SESSION["usuari"] = $usuari;
-        $_SESSION["tipo"] =$tipo;
-         header("location: welcome.php");
-      }
-      else{$message = 'El usuari es incorrecte';}
-
-  if(isset($_SESSION["usuari"])) {
-    header("Location:index.php");
-    }
-}
+require '../includes/dologin.php';
 
 ?>
 <!DOCTYPE html>
