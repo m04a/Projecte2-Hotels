d7eace880df309ee985d0256740e3d3cd2671208
Moha2001B <molamedb@gmail.com>
2021-11-08 23:37:20 +0100

    Encriptar passwords

--- Pagina/Pagina/vistes/login.php
+++ Pagina/Pagina/vistes/login.php
@@ -49,9 +49,7 @@ require '../includes/dologin.php';
     <div class="container">
       <div class="row justify-content-center align-items-center">
         <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-          <?php if(!empty($_SESSION['error'])){ ?>
                   <div class='alert alert-danger'> <?php echo $_SESSION['error']; ?> </div>
-                  <?php } ?>
           <div class="card text-white bg-secondary" id="card" style="border-radius: 15px;">
             <div class="card-body p-5">
               <h2 class="text-uppercase text-center mb-5">INICIAR SESSIÓ</h2>
