36d3cff8e2a6464b159871c23194145f2f86ca01
Moha2001B <molamedb@gmail.com>
2021-11-09 17:23:36 +0100

    Encriptar passwords

--- Pagina/Pagina/vistes/ferReserva.php
+++ Pagina/Pagina/vistes/ferReserva.php
@@ -198,6 +198,8 @@ catch(PDOException $exception){
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="../utilitats/js/accordions.js"></script>
+<script src="../utilitats/js/custom.js"></script>
+
 
 <!-- Latest compiled and minified Bootstrap JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
