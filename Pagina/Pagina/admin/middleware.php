eea8dd78575de2281977c939a20de3487eccd991
Moha2001B <molamedb@gmail.com>
2021-11-10 13:00:53 +0100

    Afeit buto interesant

--- Pagina/Pagina/admin/middleware.php
+++ Pagina/Pagina/admin/middleware.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
       if($_SESSION["tipo"]!="admin"){
-      	echo "No tens permisos per accedir aquesta pagina! Es farà una redirecció en 5 segons...";
-      	header('Refresh: 5; URL=../index.php');
+                header('Location: middlewarefilter.php');
                 }
 ?>
