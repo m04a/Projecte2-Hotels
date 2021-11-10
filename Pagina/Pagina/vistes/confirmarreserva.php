f0c5765253858e375c674b1f753b3ab47b29fdf7
Moha2001B <molamedb@gmail.com>
2021-11-11 00:18:38 +0100

    Tret try

--- Pagina/Pagina/vistes/confirmarreserva.php
+++ Pagina/Pagina/vistes/confirmarreserva.php
@@ -171,7 +171,30 @@ if($_POST){
  </form>
     </div>
     <div class="col-sm">
-      One of three columns
+ <table class='table table-dark table-hover table-responsive table-bordered'>
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+  </tbody>
+</table>
     </div>
   </div>
 </div>
