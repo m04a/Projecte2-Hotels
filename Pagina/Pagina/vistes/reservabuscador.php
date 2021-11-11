475f8d1e0d7fb31d98d27d86ffcb7f462765b777
nemesxv <lupenkov14@gmail.com>
2021-11-11 15:44:48 +0100

    movido $var->format a bindparam

--- Pagina/Pagina/vistes/reservabuscador.php
+++ Pagina/Pagina/vistes/reservabuscador.php
@@ -75,13 +75,13 @@ LA HABITACIÓ NO ESTÁ RESERVADA EN ELS PERIODES DEMANATS **/
     echo '<div class="row">';
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
         extract($row); 
-        $query2 = "SELECT COUNT(idtipo) FROM reserva WHERE finicio >= :finicio AND ffin <= :ffin and idtipo = :idtipo";
-        $result = $conn->prepare($query2); 
-        $result->bindParam(':finicio', $finicio->format('Y/m/d'));
-        $result->bindParam(':ffin', $ffin->format('Y/m/d'));
-        $result->bindParam(':idtipo', $idtipo);
-        $result->execute(); 
-        $number_of_rows = $result->fetch(); 
+        //$query2 = "SELECT COUNT(idtipo) FROM reserva WHERE finicio >= :finicio AND ffin <= :ffin and idtipo = :idtipo";
+        //$result = $conn->prepare($query2); 
+        //$result->bindParam(':finicio', $finicio->format('Y/m/d'));
+        //$result->bindParam(':ffin', $ffin->format('Y/m/d'));
+        //$result->bindParam(':idtipo', $idtipo);
+        //$result->execute(); 
+        //$number_of_rows = $result->fetchAll(); 
         //$query2 = "SELECT COUNT('{$idtipo}') FROM reserva WHERE finicio <= '{$from}' AND ffin => '{$to}'";
         //$stmt1 = $conn->prepare($query2); 
         //$resultat = mysql_fetch_row($stmt1);
@@ -92,7 +92,7 @@ LA HABITACIÓ NO ESTÁ RESERVADA EN ELS PERIODES DEMANATS **/
          <div class="col-lg-4">
                     <div class="trainer-item">
                         <?php
-                         print_r($number_of_rows);
+                         //print_r($number_of_rows);
                          echo $finicio;
                          echo $ffin;
                          echo $idtipo;
