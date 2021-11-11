f0799bff244771a88f0af74f0ce48aee48e5eb5d
nemesxv <lupenkov14@gmail.com>
2021-11-11 15:15:45 +0100

    intento de filtrado de reserva

--- Pagina/Pagina/vistes/reservabuscador.php
+++ Pagina/Pagina/vistes/reservabuscador.php
@@ -56,7 +56,11 @@
     require '../includes/conectar_DB.php';
 if(isset($_POST["reservaBuscar"])){
 $to = $_POST["to"];
+$ffin = new DateTime($to);
+$ffin->format('Y/m/d');
 $from = $_POST["from"];
+$finicio = new DateTime($from);
+$finicio->format('Y/m/d');
 $nhabitacio = $_POST["nhabitacio"];
 $npersones = $_POST["npersones"];
     if(!empty($to) && !empty($from) && !empty($nhabitacio) && !empty($npersones)){
@@ -72,6 +76,13 @@ LA HABITACIÓ NO ESTÁ RESERVADA EN ELS PERIODES DEMANATS **/
     echo '<div class="row">';
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
         extract($row); 
+        $query2 = "SELECT COUNT(idtipo) FROM reserva WHERE finicio >= :finicio AND ffin <= :ffin and idtipo = :idtipo";
+        $result = $conn->prepare($query2); 
+        $result->bindParam(':finicio', $finicio);
+        $result->bindParam(':ffin', $ffin);
+        $result->bindParam(':idtipo', $idtipo);
+        $result->execute(); 
+        $number_of_rows = $result->fetch(); 
         //$query2 = "SELECT COUNT('{$idtipo}') FROM reserva WHERE finicio <= '{$from}' AND ffin => '{$to}'";
         //$stmt1 = $conn->prepare($query2); 
         //$resultat = mysql_fetch_row($stmt1);
@@ -81,6 +92,12 @@ LA HABITACIÓ NO ESTÁ RESERVADA EN ELS PERIODES DEMANATS **/
     
          <div class="col-lg-4">
                     <div class="trainer-item">
+                        <?php
+                         print_r($number_of_rows);
+                         echo $finicio;
+                         echo $ffin;
+                         echo $idtipo;
+                        ?>
                         <div class="image-thumb">
                             <img src="../utilitats/imatges/product-2-720x480.jpg" alt="">
                         </div>
