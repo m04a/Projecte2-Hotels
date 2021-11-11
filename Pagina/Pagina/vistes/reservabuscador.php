d47e014021af33067d2ec74f253190b59e35ba18
nemesxv <lupenkov14@gmail.com>
2021-11-11 15:57:41 +0100

    añadido format al echo (devuelto el echo)

--- Pagina/Pagina/vistes/reservabuscador.php
+++ Pagina/Pagina/vistes/reservabuscador.php
@@ -123,7 +123,7 @@ LA HABITACIÓ NO ESTÁ RESERVADA EN ELS PERIODES DEMANATS **/
                                          </div>
                                     </div>
                                     <div class="alert alert-success" role="alert">
-                                    De: <?php print_r($finicio);?> | Fins: <?php //echo $ffin;?>
+                                    De: <?php echo $finicio->format('Y/m/d');?> | Fins: <?php echo $ffin->format('Y/m/d');?>
                                     </div>
                                     </form>
                                 </li>  
