42e2bb1580793bc8357a583b1128ea73bf5a7eff
Moha2001B <molamedb@gmail.com>
2021-11-12 15:49:59 +0100

    Afegides sessions

--- Pagina/Pagina/vistes/finalitzarreserva.php
+++ Pagina/Pagina/vistes/finalitzarreserva.php
@@ -98,6 +98,7 @@ require '../includes/conectar_DB.php';
                   echo "No s'ha pogut realitzar la reserva";
                  // header('Refresh: 5; URL=reservabuscador.php');  
                  } ?></div>
+<div id="contenidorpdf">
   <div class="container">
     <div class="row">
    <div class="col-sm">
@@ -110,9 +111,8 @@ require '../includes/conectar_DB.php';
     <tr>
         <td>Nom d'habitació</td>
             <td><?php echo $nomhabitacio; ?></td>
-            <td><?php echo $usuari; ?></td>
             <td>Usuari</td>
-            <td><?php echo $_SESSION["usuari"]; ?></td>
+            <td><?php echo $usuari; ?></td>
     </tr>
     <tr>
         <td>Data d'inici</td>
@@ -151,54 +151,14 @@ require '../includes/conectar_DB.php';
 </table> 
     </div>
   </div>
-<a href="javascript:demoFromHTML()" class="button">Run Code</a>
+</div>
+<a href="javascript:imprimirPDF()" type="button "class="btn btn-secondary">Run Code</a>
 <div id="content">
     <h1>  
         We support special element handlers. Register them with jQuery-style.
     </h1>
 </div></div>
-<script>
-    function demoFromHTML() {
-        var pdf = new jsPDF('p', 'pt', 'letter');
-        // source can be HTML-formatted string, or a reference
-        // to an actual DOM element from which the text will be scraped.
-        source = $('#content')[0];
 
-        // we support special element handlers. Register them with jQuery-style 
-        // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
-        // There is no support for any other type of selectors 
-        // (class, of compound) at this time.
-        specialElementHandlers = {
-            // element with id of "bypass" - jQuery style selector
-            '#bypassme': function (element, renderer) {
-                // true = "handled elsewhere, bypass text extraction"
-                return true
-            }
-        };
-        margins = {
-            top: 80,
-            bottom: 60,
-            left: 40,
-            width: 522
-        };
-        // all coords and widths are in jsPDF instance's declared units
-        // 'inches' in this case
-        pdf.fromHTML(
-            source, // HTML string or DOM elem ref.
-            margins.left, // x coord
-            margins.top, { // y coord
-                'width': margins.width, // max width of content on PDF
-                'elementHandlers': specialElementHandlers
-            },
-
-            function (dispose) {
-                // dispose: object with X, Y of the last line add to the PDF 
-                //          this allow the insertion of new lines after html
-                pdf.save('Test.pdf');
-            }, margins
-        );
-    }
-</script>
      <!-- *** Footer inici *** -->
      <?php
 	 include '../includes/footer.php';
