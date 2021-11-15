b44d7226e67b45ddf5d83809d0cc1fa2f2aafa0e
nemesxv <lupenkov14@gmail.com>
2021-11-15 15:38:03 +0100

    cambiado script de de pdf a html2pdf

--- Pagina/Pagina/vistes/finalitzarreserva.php
+++ Pagina/Pagina/vistes/finalitzarreserva.php
@@ -211,9 +211,8 @@ require '../includes/conectar_DB.php';
             </tr>
         </tbody>
     </table>
-</div> 
-<a href="javascript:generatePDF()" class="btn btn-primary">Imprimir PDF</a>-->
-<button onclick="generatePDF()">Imprimir PDF</button>
+</div> -->
+<a href="javascript:generatePDF()" class="btn btn-primary">Imprimir PDF</a>
      <!-- *** Footer inici *** -->
      <?php
 	 include '../includes/footer.php';
