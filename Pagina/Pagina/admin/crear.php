6f52604dd563114606ed0a4580d993863f6fdb9e
Moha2001B <molamedb@gmail.com>
2021-11-15 17:34:11 +0100

    Afegides sessions

--- Pagina/Pagina/admin/crear.php
+++ Pagina/Pagina/admin/crear.php
@@ -146,7 +146,7 @@ if(isset($_POST["crearusuari"])){
     
     <body> 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand">ADMIN</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -166,10 +166,6 @@ if(isset($_POST["crearusuari"])){
     <a class="nav-link" href="llistarusuaris.php">Llistar Usuaris</a>
   </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 <?php if(!empty($message)): ?>
