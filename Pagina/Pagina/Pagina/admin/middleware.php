c5cd399dbfa18604924eec5841af2e35a3b9796d
nemesxv <lupenkov14@gmail.com>
2021-11-16 18:52:04 +0100

    cambiadas las rutas a conectar_db

--- Pagina/Pagina/Pagina/admin/middleware.php
+++ Pagina/Pagina/Pagina/admin/middleware.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
       if($_SESSION["tipo"]="admin"){
-            header('Location: crear.php');
+            
       }elseif($_SESSION["tipo"]="gestor"){
-            header('Location: middlewarefilter.php');
+           
       }else{
             header('Location: middlewarefilter.php');
       }
