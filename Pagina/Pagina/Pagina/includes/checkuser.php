b7b0f9ea3230816ff1eef5a727c84077c860f304
Moha2001B <molamedb@gmail.com>
2021-11-17 18:05:16 +0100

    mmmmmmm

--- Pagina/Pagina/Pagina/includes/checkuser.php
+++ Pagina/Pagina/Pagina/includes/checkuser.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
       if(isset($_SESSION["tipo"])){
-           header('Location: URL=index.php?r=index.php');       
+           header('Location: index.php?r=index.php');       
       }
 ?>
