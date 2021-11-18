92e318421b2d814d598b67b6bf85c144eaf4381d
nemesxv <lupenkov14@gmail.com>
2021-11-18 16:59:22 +0100

    añadido contador de edad

--- Pagina/Pagina/Pagina/includes/doregistre.php
+++ Pagina/Pagina/Pagina/includes/doregistre.php
@@ -33,22 +33,58 @@ if ($password != $passwordc) {
 	  $password = password_hash($password, PASSWORD_DEFAULT);
     $sql = "INSERT INTO usuario (usuari,password,nombre,apellidos,email,fechanacimiento,sexo) VALUES ('$usuari','$password','$nombre','$apellidos','$email','$fechanacimiento',$sexo);";
     $stmt = $conn->prepare($sql);
-    $stmt->bindParam('$usuari', $_POST['usuari']);
-    $stmt->bindParam('$password', $_POST['password']);
-    $stmt->bindParam('$email', $_POST['email']);
-    $stmt->bindParam('$nombre', $_POST['nombre']);
-    $stmt->bindParam('$apellidos', $_POST['apellidos']);
-    $stmt->bindParam('$sexo', $_POST['sexo']);
-
-    $fechanacimiento = date('Y-m-d', strtotime(str_replace('-', '/', $fechanacimiento))); 
-    $stmt->bindParam('$fechanacimiento', $_POST['fechanacimiento']);
-
+      if(empty($usuari)){
+          $stmt->bindParam(':usuari', $null);
+      }else{
+          $stmt->bindParam(':usuari', $usuari);
+      }
+      if(empty($password)){
+          $stmt->bindParam(':password', $null);
+      }else{
+          $stmt->bindParam(':password', $password);
+      }
+      if(empty($nombre)){
+          $stmt->bindParam(':nombre', $null);
+      }else{
+          $stmt->bindParam(':nombre', $nombre);
+      }
+      if(empty($apellidos)){
+          $stmt->bindParam(':apellidos', $null);
+      }else{
+          $stmt->bindParam(':apellidos', $apellidos);
+      }
+      if(empty($fechanacimiento)){
+          $stmt->bindParam(':fechanacimiento', $null);
+      }else{
+        $fechanacimiento = date('Y-m-d', strtotime(str_replace('-', '/', $fechanacimiento))); 
+        $stmt->bindParam('$fechanacimiento', $_POST['fechanacimiento']);
+      }
+      if(empty($sexo)){
+          $stmt->bindParam(':sexo', $null);
+      }else{
+          $stmt->bindParam(':sexo', $sexo);
+      }
+      if(empty($email)){
+          $stmt->bindParam(':email', $null);
+      }else{
+          $stmt->bindParam(':email', $email);
+      }
   }
+  //date in mm/dd/yyyy format; or it can be in other formats as well
+  $birthDate = $fechanacimiento;
+  //explode the date to get month, day and year
+  $birthDate = explode("/", $birthDate);
+  //get age from date or birthdate
+  $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+    ? ((date("Y") - $birthDate[2]) - 1)
+    : (date("Y") - $birthDate[2]));
+  $message = $age;
+
 
     if ($stmt->execute()) {
-      $message = 'El usuari ha sigut creat';
+      $message .= 'El usuari ha sigut creat';
     } else {
-      $message = 'Ha hagut algun error';
+      $message .= 'Ha hagut algun error';
     }
   }
 ?>
