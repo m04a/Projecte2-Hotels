e1673fe3f54d812d4ee18eed11c718ff89973859
nemesxv <lupenkov14@gmail.com>
2021-11-18 18:54:31 +0100

    añadido contador de edad acabado

--- Pagina/Pagina/Pagina/src/vistes/registre.php
+++ Pagina/Pagina/Pagina/src/vistes/registre.php
@@ -60,7 +60,7 @@ require '../includes/checkuser.php';
                  <div class="form-outline mb-4">
                  <label class="form-label">Nom</label>
                   <input type="text" name="nombre" class="form-control form-control-lg" 
-                  value="<?php echo $_POST['nombre'];  ?>/>
+                  value="<?php echo $_POST['nombre'];  ?>"/>
                 </div>
                 <div class="form-outline mb-4">
                   <label class="form-label">Cognom</label>
