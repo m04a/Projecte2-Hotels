c759afa350477ec5cec65aee14167ea981982890
nemesxv <lupenkov14@gmail.com>
2021-11-18 17:05:26 +0100

    añadido contador de edad

--- Pagina/Pagina/Pagina/includes/doregistre.php
+++ Pagina/Pagina/Pagina/includes/doregistre.php
@@ -73,7 +73,7 @@ if ($password != $passwordc) {
   //date in mm/dd/yyyy format; or it can be in other formats as well
   $birthDate = $fechanacimiento;
   //explode the date to get month, day and year
-  $birthDate = explode("/", $birthDate);
+  $birthDate = explode("-", $birthDate);
   //get age from date or birthdate
   $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
     ? ((date("Y") - $birthDate[2]) - 1)
