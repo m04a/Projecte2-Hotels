4b8d0caa59df6e813783d5c2f2fbae2ad28c99b4
Moha2001B <molamedb@gmail.com>
2021-11-18 19:11:00 +0100

    Merge

--- Pagina/Pagina/Pagina/src/vistes/configuracio.php
+++ Pagina/Pagina/Pagina/src/vistes/configuracio.php
@@ -44,14 +44,14 @@
 require '../includes/conectar_DB.php';
    
 
-$action = isset($_GET['action']) ? $_GET['action'] : "";
- 
+$usuari = $_SESSION["usuari"];
+
 // if it was redirected from delete.php
 if($action=='deleted'){
     echo "<div class='alert alert-success'>Record was deleted.</div>";
 } 
 // select all data
-$query = "SELECT idtipo, precio, imagen, m2, cantidad, persmax, descripcion, nom FROM tipo ORDER BY idtipo DESC";
+$query = "SELECT numpers,idtipo,finicio,ffin,usuario,preciototal,canthab FROM reserva WHERE usuario='$usuari' ORDER BY idtipo DESC";
 $stmt = $conn->prepare($query); 
 $stmt->execute();
  
@@ -65,18 +65,18 @@ echo "<a href='crear.php' class='btn btn-primary m-b-1em'>Crear un nou tipus</a>
 if($num>0){
  
     //start table
-echo "<table class='table table-hover table-responsive table-bordered'>";
+echo "<table class='table table-light table-hover table-responsive table-bordered'>";
  
     //creating our table heading
     echo "<tr>
-        <th>Preu</th>
+        <th>Numero de persones</th>
         <th>Imatge</th>
         <th>Metres cuadrats</th>
         <th>Cantitat d'habitacions</th>
         <th>Persones maximes</th>
-        <th>Descripció</th>
-        <th>Tipus</th>
-        <th>Accions</th>
+        <th>Persones maximes</th>
+        <th>Persones maximes</th>
+
     </tr>";
  
     // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
@@ -88,19 +88,15 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
  
     // creating new table row per record
     echo "<tr>
-        <td>{$precio}</td>
-        <td>{$imagen}</td>
-        <td>{$m2}</td>
-        <td>{$cantidad}</td>
-        <td>{$persmax}</td>
-        <td>{$descripcion}</td>
-        <td>{$nom}</td>
+        <td>{$numpers}</td>
+        <td>{$idtipo}</td>
+        <td>{$finicio}</td>
+        <td>{$ffin}</td>
+        <td>{$usuario}</td>
+        <td>{$preciototal}</td>
+        <td>{$canthab}</td>
         <td>";
-            // read one record
-            echo "<a href='veure-un.php?idtipo={$idtipo}' class='btn btn-info m-r-1em'>Llegir</a>";
- 
-            // we will use this links on next part of this post
-            echo "<a href='actualitzar.php?idtipo={$idtipo}' class='btn btn-primary m-r-1em'>Editar</a>";
+xt part of this post
  
             // we will use this links on next part of this post
             echo "<a href='#' onclick='esborrar({$idtipo});'  class='btn btn-danger'>Esborrar</a>";
