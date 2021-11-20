768c93c2ac2769489d0a822bb52c2746de308a47
Moha2001B <molamedb@gmail.com>
2021-11-20 14:09:09 +0100

    Millora inde

--- Pagina/Pagina/Pagina/src/vistes/confirmarreserva.php
+++ Pagina/Pagina/Pagina/src/vistes/confirmarreserva.php
@@ -52,7 +52,7 @@ include '../includes/crearReserva.php';
  <table class='table table-dark table-hover table-responsive table-bordered'>
     <tr>
             <th colspan="2"><h3>Revisa les teves dades de registre
-                <?php if(!empty($missatge)){?><div class="alert alert-warning" role="alert"><?php $missatge;?></div><?php }?>
+                <?php if(!empty($missatge)){?><div class="alert alert-warning" role="alert"><?php echo $missatge;?></div><?php }?>
             </h3></th>
     </tr>
     <tr>
