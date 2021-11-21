5e72f4b762210e4724b3d5ca23c2175a32ef81b6
nemesxv <lupenkov14@gmail.com>
2021-11-21 06:38:37 +0100

    añadido favicon

--- Pagina/Pagina/Pagina/src/vistes/habitacions.php
+++ Pagina/Pagina/Pagina/src/vistes/habitacions.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
-
+    <link rel='shortcut icon' type='image/x-icon' href='../utilitats/imatges/favicon.ico' />
     <title>Pagina Hotel| Termes i condicions </title>
 
     <link rel="stylesheet" type="text/css" href="../utilitats/css/font-awesome.css">
